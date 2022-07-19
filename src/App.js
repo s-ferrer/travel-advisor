422179c7d00d3b56ae416777dfc31b482c0331f1
s-ferrer <saraferrer@gmail.com>
2022-07-19 14:43:49 +0200

    Create environmet variable

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { CssBaseline, Grid } from "@material-ui/core";
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
+require("dotenv").config();
 
 const App = () => {
   return (
